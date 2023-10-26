80137da84b3eec6c78c76ce301cffe8144c44a69
Ashadobler <asha.dobler@stud.fhgr.ch>
2023-10-26 14:40:15 +0200

    Dynamischer Text fertig

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 const changeFont = document.querySelector("#changeFont");
 const styleButtons = document.querySelector("#styleButtons");
 const widthButtons = document.querySelector("#widthButtons");
-
+const colorButtons = document.querySelector("#colorButtons");
 
 ////////////weight buttons /////////////////////////////////
 ////////////////////////////////////////////////////////////
@@ -62,3 +62,79 @@ function toggleFontStyle() {
 ////////////////////////////////////////////////////////////
 ////////////End style buttons /////////////////////////////
 
+
+
+////////////color buttons /////////////////////////////////
+////////////////////////////////////////////////////////////
+const colorValues = [
+    { name: "Peach", color: "#F0866D" },
+    { name: "Blue", color: "#6da8f0" },
+    { name: "Purple", color: "#a0049e" }
+];
+
+colorValues.forEach(({ name, color }) => {
+    const button = document.createElement('button');
+    button.innerText = name;
+    button.value = color;
+
+    button.addEventListener('click', () => {
+        changeFontColor(color);
+    });
+
+    colorButtons.appendChild(button);
+});
+
+function changeFontColor(color) {
+    changeFont.style.color = color;
+}
+
+////////////////////////////////////////////////////////////
+////////////End color buttons /////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+////////////Dynamischer hover Text /////////////////////////
+
+let mouseX = "";
+let mouseY = "";
+let screen_Width = "";
+let screen_Height = "";
+let propMouseX = "";
+let propMouseY = "";
+
+const mouseDiv = document.querySelector("#mousePosition");
+
+document.addEventListener('mousemove', function(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+
+    propMouseX = ((mouseX/screen_Width)*50)+50;
+    propMouseY = (mouseY/screen_Height)*100;
+
+    mouseDiv.innerHTML = `rel X Position: ${propMouseX}, rel Y Position: ${propMouseY}`;
+
+    // Das <span>-Element auswählen
+    let resizeableText = document.querySelector("#resizeText");
+
+    // Die Schriftgröße des <span>-Elements auf 20px setzen
+    resizeableText.style.fontVariationSettings = `'wdth' ${propMouseX}`;
+    resizeableText.style.fontSize = propMouseY + "px";;
+
+});
+
+
+// Eventlistener für das resize-Event hinzufügen
+window.addEventListener('resize', function() {
+    screen_Width = window.innerWidth;
+    screen_Height = window.innerHeight;
+    //mouseDiv.innerHTML = screenWidth + " " + screenHeight;
+
+});
+
+// Optional: Um die Werte sofort beim Laden der Seite anzuzeigen
+screen_Width = window.innerWidth;
+screen_Height = window.innerHeight;
+
+
+////////////////////////////////////////////////////////////
+////////////End Dynamischer hover Text /////////////////////////
